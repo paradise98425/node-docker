ec7146952d9197acdb6aed35d4f1cfab294060bc
paradise98425 <paradise98425@gmail.com>
2021-11-08 23:09:14 +0100

    docker-node setup with nodemon

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello world!!!</h1>")
+})
+
+const port = process.env.port || 3000;
+
+app.listen(port, ()=> console.log(`listening on port ${port}`))
