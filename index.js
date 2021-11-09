c48995c28381a42ef8fc642889445bc79b760542
paradise98425 <paradise98425@gmail.com>
2021-11-09 14:09:16 +0100

    separate yml files for development and production

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello world!!!</h1>")
+    res.send("<h1>Hello world!</h1>")
 })
 
 const port = process.env.port || 3000;
