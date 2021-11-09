958988ffe06324b8b86b39ae856d088aff469c41
paradise98425 <paradise98425@gmail.com>
2021-11-09 20:23:58 +0100

    node-docker setup

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const app = express();
 
+//GET request
+//POST
+//PUT
+//DELETE 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello world!</h1>")
+    // Query to the database
+    res.send("<h1>Hello world</h1>")
 })
 
 const port = process.env.port || 3000;
